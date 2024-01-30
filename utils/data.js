dcf23001754c3b45e970b29cf16812d79815332f
danielgoss22 <daniel.goss22@gmail.com>
2024-01-30 08:18:17 -0700

    added seed and data code

---
+++ utils/data.js
@@ -0,0 +1,23 @@
+const users = [
+  {
+    username: "lernantino",
+    email: "lernantino@gmail.com",
+  },
+  {
+    username: "daniel",
+    email: "d@gmail.com",
+  },
+  {
+    username: "joe",
+    email: "j@gmail.com",
+  },
+  {
+    username: "Haley",
+    email: "h@gmail.com",
+  },
+  {
+    username: "Sarah",
+    email: "s@gmail.com",
+  },
+];
+module.exports = users;
---
+++ utils/seed.js
@@ -0,0 +1,25 @@
+const connection = require("../config/connection");
+const { User } = require("../models");
+const user = require("./data");
+
+connection.once("open", async () => {
+  let userCheck = await connection.db
+    .listCollections({ name: "user" })
+    .toArray();
+  if (userCheck.length) {
+    await connection.dropCollection("user");
+  }
+
+  let thoughtCheck = await connection.db
+    .listCollections({ name: "thought" })
+    .toArray();
+  if (thoughtCheck.length) {
+    await connection.dropCollection("thought");
+  }
+
+  await User.insertMany(users);
+
+  console.table(user);
+  console.info("Seeding Done!");
+  process.exit(0);
+});
